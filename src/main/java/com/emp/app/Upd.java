5b68ba124b18d6c190105e21de5e3288767ed73b
shevchuk-d <authentic.d@gmail.com>
2016-09-02 09:13:14 +0300

    in progress..

---
+++ src/main/java/com/emp/app/Upd.java
@@ -0,0 +1,16 @@
+package com.emp.app;
+
+/**
+ * Created by sh on 9/1/2016.
+ */
+public class Upd {
+    private String limit = "5";
+
+    public String getLimit() {
+        return limit;
+    }
+
+    public void setLimit(String limit) {
+        this.limit = limit;
+    }
+}
